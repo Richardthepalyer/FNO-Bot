b76bf06e92427dc7a610d13238e61647ad67251d
Richardthepalyer <129241031+Richardthepalyer@users.noreply.github.com>
2026-06-01 13:54:19 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -27,7 +27,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "James 🫥",
         // Activity type number (0 = Playing).
-        type: 2, 
+        type: 3, 
       },
     ],
   },
